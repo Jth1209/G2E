3d8b4e8ed3ebda95f929788dc84f5d8f0d220d58
Jth1209 <xkgshand@naver.com>
2024-12-04 18:09:41 +0900

    first commit

---
+++ src/main/java/edu/du/gte/GteApplication.java
@@ -0,0 +1,13 @@
+package edu.du.gte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GteApplication.class, args);
+    }
+
+}
